ffd98789b7947f43c84ab8ec663544813c068db1
Aluno <Aluno@SNCTM202LABF08>
2024-03-13 22:07:40 -0300

    Adicionar arquivos de projeto.

---
+++ ControleEPIApp/ControleEPIApp/View/PageListarEPI.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ControleEPIApp.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControleEPIApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageListarEPI : ContentPage
+    {
+        public PageListarEPI()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            lsvEPI.ItemsSource = MySQLCon.ListarEPI();
+            lsvEPIVencida.ItemsSource = MySQLCon.ListarEPIVencida();
+        }
+    }
+}
